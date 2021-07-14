2ad9238822b855e1e82a4df4813595f30e0efcbb
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2021-07-14 16:27:41 +0800

    Number of new cases per day of a country

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
-import Chart from 'chart.js/auto'
+import { Bar } from 'react-chartjs-2';
 
 function Home() {
 
     const [data, setData] = useState('')
+
+    // For graph x axis (Time)
+    const [dates, setDates] = useState([])
+    const [confirmed, setConfirmed] = useState([])
+    const [recovered, setRecovered] = useState([])
+    const [deaths, setDeaths] = useState([])
 
     const [country, setCountry] = useState('malaysia')
     const handleCountryChange = ({ target }) => {
@@ -36,19 +42,56 @@ function Home() {
 
         e.preventDefault()
 
-        const response = await fetch(`https://api.covid19api.com/country/${country}/status/confirmed?from=${secondDate}T00:00:00Z&to=${firstDate}T00:00:00Z`)
+        const response = await fetch(`https://api.covid19api.com/country/${country}?from=${secondDate}T00:00:00Z&to=${firstDate}T00:00:00Z`)
 
         if (response.ok) {
             const jsonResponse = await response.json()
 
-            const number = jsonResponse.map(snaps => snaps.Cases)
+            setDates(jsonResponse.map( elem => elem.Date.slice(0, 10) ))
+            // casesPerDay is a helper function
+            setConfirmed(casesPerDay(jsonResponse.map( elem => elem.Confirmed )))
 
-            console.log(jsonResponse[0].Country)
-            
-            setData(number)
+            setShow(true)
         }
 
     }
+
+    const [show, setShow] = useState(false)
+
+    const dataChart = {
+        labels: dates,
+        datasets: [
+          {
+            label: '# of Confirmed cases',
+            data: confirmed,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)'
+            ],
+            borderWidth: 1,
+          },
+        ],
+      };
+      
+      const options = {
+        title: {
+            display: true,
+            text: 'This is a title',
+            fontSize: 20
+        },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      };
+      
 
     return (
         <div className="d-flex w-100 flex-column m-auto mb-5" style={{ maxWidth: '900px' }}>
@@ -82,9 +125,9 @@ function Home() {
                 </Form>
             </div>
 
-            <div className="mx-5"> 
-                <p>Homepage</p>
-                { data && data.map( (element, index) => <p key={index} >{element}</p>)}
+            <div className="mx-5 my-5 text-center"> 
+                { !show && <h6>Your graph will appear here. <br />Please pick a country 🌍 and a span of time ⌛</h6> }
+                { show && <Bar data={dataChart} options={options} /> }
             </div>
         </div>
     )
@@ -98,6 +141,18 @@ const dateFormat = (time) => {
 
     return yyyy + '-' + mm + '-' + dd;
 
+}
+
+const casesPerDay = (cumulativeCases) => {
+
+    const casesPerDay = []
+
+    for (let i = 1; i < cumulativeCases.length; i++) {
+        const numOfCase = cumulativeCases[i] - cumulativeCases[i-1]
+        casesPerDay.push(numOfCase)
+    }
+
+    return casesPerDay
 }
 
 export default Home
