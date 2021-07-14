512ada30190d98bea7df705e15a4d32cdad726dc
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2021-07-14 15:21:59 +0800

    Create react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Nav from 'react-bootstrap/Nav'
+import About from './pages/About/About'
+import Home from './pages/Home/Home'
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav className="fixed-bottom justify-content-center py-1 bg-dark">
+          <Nav.Item>
+            <Nav.Link href="/" className="text-white" style={{ fontFamily: 'Pacifico' }}>
+              COVID-19 Data go pew pew 📊
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/about" className="text-white">
+              About
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
